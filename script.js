cd4e4d649cdd9f5764aa7cfd43ffeb675aac0433
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-01-30 10:58:35 +0600

    adding more javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function ticketHandle(product,isIncrease) {
+function ticketHandle(product, isIncrease) {
     const ticketInput = document.getElementById(product + 'Input');
     const ticketNewInput = parseInt(ticketInput.value);
     let ticketLastInput = ticketNewInput;
@@ -20,26 +20,52 @@ function ticketHandle(product,isIncrease) {
     subTotal();
 }
 
+
 function subTotal() {
     const firstClassTicketCount = ticketInput('firstClass');
     const economyTicketCount = ticketInput('economy');
 
-    const  subTotal = firstClassTicketCount * 150 + economyTicketCount * 100;
-     document.getElementById('subTotal').innerText = subTotal;
+    const subTotal = firstClassTicketCount * 150 + economyTicketCount * 100;
+    document.getElementById('subTotal').innerText = subTotal;
 
-     const tax =Math.round( subTotal * 0.1);
-     document.getElementById('tax').innerText = tax;
+    const tax = Math.round(subTotal * 0.1);
+    document.getElementById('tax').innerText = tax;
 
-     const grandTotal = subTotal + tax;
-     document.getElementById('total').innerText = grandTotal;
+    const grandTotal = subTotal + tax;
+    document.getElementById('total').innerText = grandTotal;
 }
 
 function ticketInput(product) {
-    const ticketInput = document.getElementById( product + 'Input');
-    const  ticketCount = parseInt(ticketInput.value);
+    const ticketInput = document.getElementById(product + 'Input');
+    const ticketCount = parseInt(ticketInput.value);
     return ticketCount;
 }
 
 function bookNow() {
-    alert('Thank you for your purchase..!!')
+    const details = document.getElementById('details');
+    details.style.display = "none";
+    let confirmation = document.getElementById('confirmation');
+    confirmation.style.display = "block";
+
+    const firstClassTicketCount = ticketInput('firstClass');
+    const economyTicketCount = ticketInput('economy');
+
+    const firstClassPieces = document.getElementById('firstClassPieces');
+    firstClassPieces.innerText = firstClassTicketCount;
+    const economyPieces = document.getElementById('economyPieces');
+    economyPieces.innerText = economyTicketCount;
+
+    const totalPieces = document.getElementById('totalPieces');
+    totalPieces.innerText = firstClassTicketCount + economyTicketCount;
+
+    const firstClassPrice = document.getElementById('firstClassPrice');
+    firstClassPrice.innerText = firstClassTicketCount * 150;
+
+    const economyClassPrice = document.getElementById('economyClassPrice');
+    economyClassPrice.innerText = economyTicketCount * 100;
+
+    const total = document.getElementById('totalPrice');
+    const totalPrice = firstClassTicketCount * 150 + economyTicketCount * 100;
+    const totalPriceWithTax = totalPrice * 0.1;
+    total.innerText = totalPrice + totalPriceWithTax;
 }
